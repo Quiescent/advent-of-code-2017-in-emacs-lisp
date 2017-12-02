183ecc51a178910d42b33f3a23eeff21eb1b1457
Edward John Steere <edward.steere@gmail.com>
2017-12-02 07:51:13 +0200

    Adds a setup script for developing advent of code questions
     - setup.el: Ensure that Flycheck uses the current load path instead
       of whatever load path it was using so that I don't get spurious
       errors while developing

---
+++ setup.el
@@ -0,0 +1,10 @@
+;;; setup --- A buffer which should be evalled before I get hacking -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(setq-default flycheck-emacs-lisp-load-path load-path)
+
+(provide 'setup)
+;;; setup ends here
