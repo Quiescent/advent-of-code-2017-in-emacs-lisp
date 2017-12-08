7e95519f8e6e00cc6e02bee22e457ffbe1855fea
Edward John Steere <edward.steere@gmail.com>
2017-12-08 07:34:25 +0200

    Solves day 5
     - day5.el:
       * `follow-instruction' follow a jump instruction
       * `follow-instruction-2' follow a jump instruction with an
         additional rule to jump 2 less if the jump is greater than two.

--- day5.el
+++ day5.el
@@ -17,16 +17,73 @@
 ;;; Code:
 
 (require 'parse)
+(eval-when-compile
+  (require 'cl))
+
+;; part 1:
+
+(cl-map 'vector #'string-to-number (apply #'vector (parse-lines "0
+3
+0
+1
+-3")))
+
+(defun follow-instruction (instructions idx)
+  "Follow the jump at IDX in INSTRUCTIONS.
+Increment instruction at IDX afterwards."
+  (if (>= (+ idx (aref instructions idx))
+          (length instructions))
+      nil
+    (let ((move (aref instructions idx)))
+      (aset instructions idx (1+ move))
+      (+ idx move))))
 
 (defun day5-part-1 (input)
   "Solve day 5 for INPUT."
   (interactive "sInput: ")
-  )
+  (message "%s"
+           (let ((cnt 0)
+                 (idx 0)
+                 (instructions (cl-map 'vector #'string-to-number (apply #'vector (parse-lines input)))))
+             (while (not (null idx))
+               (incf cnt)
+               (setq idx (follow-instruction instructions idx)))
+             cnt)))
+
+;; solution: 372139
+
+;; part 2:
+
+
+(defun follow-instruction-2 (instructions idx)
+  "Follow the jump at IDX in INSTRUCTIONS.
+If the jump was greater than two then subtract from the
+instruction otherwise increment it."
+  (let* ((new-idx (+ idx (aref instructions idx))))
+    (if (or (>= new-idx
+                (length instructions))
+            (< new-idx
+               0))
+        nil
+      (let ((move (aref instructions idx)))
+        (if (>= move 3)
+            (aset instructions idx (1- move))
+          (aset instructions idx (1+ move)))
+        new-idx))))
 
 (defun day5-part-2 (input)
-  "Solve day 5 part 2 for INPUT."
+  "Solve day 5 for INPUT."
   (interactive "sInput: ")
-  )
+  (message "%s"
+           (let ((cnt 0)
+                 (idx 0)
+                 (instructions (cl-map 'vector #'string-to-number (apply #'vector (parse-lines input)))))
+             (while (not (null idx))
+               (incf cnt)
+               (setq idx (follow-instruction-2 instructions idx)))
+             cnt)))
+
+;; solution: 29629538
 
 (provide 'day5)
 ;;; day5 ends here
