b07a5a2cddd9dfc3ee64fd281cde5c98fb0c0725
Edward John Steere <edward.steere@gmail.com>
2017-12-09 09:15:12 +0200

    Solve day9 and day10
     - day9.el:
       * `parse-stream' the stream of tokens parsed from the input string
       * `segments' the segments parsed from the input string
       * `parse' parse string into parse stream and segments
       * `interesting-chars' the chars which punctuate the stream
       * `split-to-next' split the stream up to an interesting char
       * `score' score the given input stream
       * `day8-part-1' implement
       * `garbage' try to use the segments parsed to get garbage out
       * `day8-part-2' implement
       * `garbage-segments' use an Emacs buffer to get the garbage out
       * `remove-nots' remove cancelled sequences from string
     - day10.el:
       * `reverse-length' reverse a length in circular sequencing starting
         at a given point
       * `day9-part-1' implement
       * `hex-numbers' the hex numbers
       * `to-hex' convert a number to hexadecimal
       * `day9-part-2' implement

--- day10.el
+++ day10.el
@@ -1,32 +1,90 @@
-;;; day10 --- a solution to day 10 -*- lexical-binding: t; -*-
+;;; day10 --- a solution to day 9 -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
-;; First Part
-
-;; Input:
-
-;; Solution:
-
-;; Second Part
-
-;; Input:
-
-;; Solution:
+;; Input: 106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118
 
 ;;; Code:
 
 (require 'parse)
+(eval-when-compile
+  (require 'cl))
+(require 'seq)
+
+;; Part 1
+
+(defun reverse-length (xs rev-len idx)
+  "Reverse the subseq of XS starting at IDX with REV-LEN length.
+
+XS is circular and REV-LEN can wrap."
+  (let* ((end        (if (> (+ rev-len idx) (length xs))
+                         (length xs)
+                       (+ rev-len idx)))
+         (from-start (if (> (+ rev-len idx) (length xs))
+                         (- rev-len (- (length xs) idx))
+                       0))
+         (rev-segment (reverse (seq-concatenate 'list
+                                                (seq-subseq xs idx end)
+                                                (seq-subseq xs 0 from-start)))))
+    (seq-concatenate 'list
+                     (seq-subseq rev-segment (- (length rev-segment) from-start))
+                     (seq-subseq xs from-start idx)
+                     (seq-subseq rev-segment 0 (- (length rev-segment) from-start))
+                     (seq-subseq xs end))))
+
+(reverse-length "this is a test" 5 2)
 
 (defun day10-part-1 (input)
-  "Solve day 10 for INPUT."
-  (interactive "sInput: "))
+  "Solve day 9 for INPUT."
+  (interactive "sInput: ")
+  (let* ((lengths (mapcar #'string-to-number (split-string input "," t)))
+         (idx 0)
+         (skip 0)
+         (numbers (let (xs)
+                    (dotimes (i 256 (reverse xs))
+                      (push i xs)))))
+    (message "%s"
+             (dolist (length lengths (apply #'* (seq-take numbers 2)))
+               (setq numbers (reverse-length numbers length idx))
+               (setq idx  (mod (+ idx (+ length skip)) (length numbers)))
+               (incf skip)))))
 
+;; (day10-part-1 "3,4,1,5") Set 256 to 5 above to test this
 
-(defun day20-part-1 (input)
-  "Solv part 2e day 10 for INPUT."
-  (interactive "sInput: "))
+;; Solution: 6909
 
+;; Part 2
+
+(defconst hex-numbers '("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "A" "B" "C" "D" "E" "F")
+  "The numbers in hexadecimal radix.")
+
+(defun to-hex (number)
+  "Convert NUMBER to hexadecimal."
+  (concat (nth (/ number 16) hex-numbers)
+          (nth (mod number 16) hex-numbers)))
+
+(defun day10-part-2 (input)
+  "Solve day 9 part 2 for INPUT."
+  (interactive "sInput: ")
+  (let* ((lengths (seq-concatenate 'list (string-to-list input) '(17 31 73 47 23)))
+         (idx 0)
+         (skip 0)
+         (numbers (let (xs)
+                    (dotimes (i 256 (reverse xs))
+                      (push i xs)))))
+    (message "%s"
+             (mapconcat #'to-hex
+                        (mapcar (lambda (sub-list) (apply #'logxor sub-list))
+                                (seq-partition
+                                 (dotimes (_ 64 numbers)
+                                   (dolist (length lengths)
+                                     (setq numbers (reverse-length numbers length idx))
+                                     (setq idx  (mod (+ idx (+ length skip)) (length numbers)))
+                                     (incf skip)))
+                                 16))
+                        ""))))
+
+;; Solution: 9d5f4561367d379cfbf04f8c471c0095
 
 (provide 'day10)
 ;;; day10 ends here
--- day9.el
+++ day9.el
@@ -1,32 +1,184 @@
-;;; day9 --- a solution to day 9 -*- lexical-binding: t; -*-
+;;; day9 --- a solution to day 8 -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
-;; First Part
-
-;; Input:
-
-;; Solution:
-
-;; Second Part
-
-;; Input:
-
-;; Solution:
+;; My mistake:
+;;  a) I thought that the second half would be more complex.
+;;  b) I thought that it wouldn't care about the content of garbage or
+;;     of segments
 
 ;;; Code:
 
 (require 'parse)
+(require 'seq)
+(eval-when-compile
+  (require 'cl))
+
+;; Part 1
+
+(defvar parse-stream nil "The stream of tokens parsed so far.")
+(defvar segments nil "The segments parsed during parsing.")
+
+(defun parse (remaining-input)
+  "Parse tokens from REMAINING-INPUT."
+  (progn
+    (while (not (string-equal remaining-input ""))
+      (pcase (split-to-next remaining-input)
+        (`(,match ,left ,right)
+         (push match parse-stream)
+         (push left segments)
+         (setq remaining-input right))))
+    (setq segments (reverse segments))
+    (progn
+      (reverse parse-stream))))
+
+(defconst interesting-chars '(?} ?{ ?> ?<) "Chars which interrupt the stream.")
+
+(defun split-to-next (remaining)
+  "Split REMAINING up to the next interesting char."
+  (let ((idx 0))
+    (while (and (< idx (length remaining))
+                (not (member (aref remaining idx)
+                             interesting-chars)))
+      (when (eq (aref remaining idx) ?!)
+        (cl-incf idx))
+      (cl-incf idx))
+    `(,(aref remaining idx)
+      ,(substring remaining 0 (1+ idx))
+      ,(substring remaining (1+ idx)))))
+
+(split-to-next "a!>,<a>,<a>")
+
+(defun score (stream depth)
+  "Traverse down STREAM adding DEPTH to score when we go deeper.
+
+DEPTH is also decremented when we go up."
+  (let ((score 0))
+    (while (not (null stream))
+      (let ((next-element (pop stream)))
+        (cond
+         ((eq next-element ?<)
+          (setq stream (cdr (seq-drop-while (lambda (x) (not (eq x ?>))) stream))))
+         ((eq next-element ?{)
+          (progn
+            (incf depth)
+            (incf score depth)))
+         ((eq next-element ?})
+          (progn
+            (decf depth))))))
+    score))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{}") 0))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{{{}}}") 0))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{{{},{},{{}}}}") 0))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{<a>,<a>,<a>,<a>}") 0))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{{<!!>},{<!!>},{<!!>},{<!!>}}") 0))
+
+(let ((parse-stream nil)
+      (segments nil))
+  (score (parse "{{<a!>},{<a!>},{<a!>},{<ab>}}") 0))
 
 (defun day9-part-1 (input)
-  "Solve day 9 for INPUT."
+  "Solve day 8 for INPUT."
   (interactive "sInput: ")
-  )
+  (let* ((lines               (parse-lines input))
+         (parse-stream        nil)
+         (segments            nil)
+         (stream              (parse (car lines))))
+    (message "%s" (score stream 0))))
+
+;; Solution: 10820
+
+;; Part 2
+
+
+(defun garbage (stream)
+  "First attempt to get garbage out of STREAM.
+Doesn't work."
+  (let ((garbage-segments ""))
+    (while (not (null stream))
+      (let ((next-element (pop stream))
+            (next-segment (pop segments)))
+        (when (eq next-element ?<)
+          (while (and stream segments
+                      (not (eq (car stream) ?>)))
+            (setq next-element (pop stream))
+            (setq next-segment (pop segments))
+            (setq garbage-segments (concat garbage-segments next-segment)))
+          (setq next-segment (pop segments))
+          (when (> (length next-segment) 1)
+            (setq garbage-segments (concat garbage-segments (substring next-segment 0 -1)))))))
+    garbage-segments))
 
 (defun day9-part-2 (input)
-  "Solve day 9 part 2 for INPUT."
+  "Solve day 8 part 2 for INPUT."
   (interactive "sInput: ")
-  )
+  (message
+   "%s"
+   (length (remove-nots (apply #'concat (garbage-segments input))))))
+
+(day9-part-2 "<random characters>")
+(day9-part-2 "<>")
+(day9-part-2 "<<<<>")
+(day9-part-2 "<{!>}>")
+(day9-part-2 "<!!>")
+(day9-part-2 "<!!!>>")
+(day9-part-2 "<{o\"i!a,<{i<a>")
+(day9-part-2 "<{o\"i!!!!a,<{i<!a>")
+(day9-part-2 "<{o\"!!>i!!!!a,<{i<!a>")
+
+(defun garbage-segments (string)
+  "Get garbage out of STRING using an Emacs buffer.
+Probably a better way to parse long strings anyway."
+  (let ((segments nil))
+    (with-temp-buffer
+      (insert string)
+      (goto-char (point-min))
+      (while (search-forward "<" nil t)
+        (let ((found nil)
+              (start (point))
+              end)
+          (while (and (null found)
+                      (search-forward ">"))
+            (let ((match (buffer-substring start (point))))
+              (when (cl-evenp (length (seq-take-while (lambda (x) (eq x ?!))
+                                                      (substring (reverse match) 1))))
+                (setq found t)
+                (setq end (point)))))
+          (push (buffer-substring start (1- end)) segments)))
+      segments)))
+
+(garbage-segments "<{o\"!!!>i!!!!a,<{i<!a>")
+
+(defun remove-nots (string)
+  "Remove the bang sequences from STRING."
+  (let ((result ""))
+    (while (not (string-equal string ""))
+      (if (eq (aref string 0) ?!)
+          (setq string (substring string 2))
+        (progn
+          (setq result (concat result (substring string 0 1)))
+          (setq string (substring string 1)))))
+    result))
+
+(remove-nots "{o\"i!!a,<{i<a")
+(remove-nots "<{o\"i!!!!a,<{i<!a>")
+
+;; Solution: 5547
 
 (provide 'day9)
 ;;; day9 ends here
