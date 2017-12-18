10ec241625b27b4bb64119cdd3c5eeb6d7dfb8b5
Edward John Steere <edward.steere@gmail.com>
2017-12-18 18:08:56 +0200

    Clean up solution to day18
     - day18.el: Document all functions and variables
       * `execute-instruction'
       * `execute-instruction-transmition': Fix the JGZ case -- it should
         check whether both are registers and not just use them as
         literals

--- day18.el
+++ day18.el
@@ -2,21 +2,56 @@
 
 ;;; Commentary:
 
+;; A failed attempt where they operated in lock-step
+;; (when (and (not (> pointer-0 program-length))
+;;                  (not (is-blocking last-instruction-result-0 channel-0)))
+;;         (setq last-instruction-result-0
+;;               (execute-instruction-transmition (aref instruction-vec
+;;                                                      pointer-0)
+;;                                                register-bank-0
+;;                                                'channel-0
+;;                                                'channel-1))
+;;         (when (eq (car last-instruction-result-0) 'SND)
+;;           (cl-incf send-count))
+;;         (if (and last-instruction-result-0
+;;                  (eq (car last-instruction-result-0)
+;;                      'JUMP))
+;;             (setq pointer-0 (+ pointer-0 (cadr last-instruction-result-0)))
+;;           (cl-incf pointer-0)))
+;;       (when (and (not (> pointer-1 program-length))
+;;                  (not (is-blocking last-instruction-result-1 channel-1)))
+;;         (setq last-instruction-result-1
+;;               (execute-instruction-transmition (aref instruction-vec
+;;                                                      pointer-1)
+;;                                                register-bank-1
+;;                                                'channel-1
+;;                                                'channel-0))
+;;         (if (and last-instruction-result-1
+;;                  (eq (car last-instruction-result-1)
+;;                      'JUMP))
+;;             (setq pointer-1 (+ pointer-1 (cadr last-instruction-result-1)))
+;;           (cl-incf pointer-1)))
+
 ;;; Code:
 
 (require 'parse)
+(eval-when-compile
+  (require 'cl))
 
 ;; Part 1:
 
 (defun is-a-number (string)
+  "Produce t if STRING represents a number."
   (string-match-p "\\([0-9]+\\)\\|-\\([0-9]+\\)" string))
 
 (defun parse-argument (argument)
+  "Parse ARGUMENT into a literal or register reference."
   (if (is-a-number argument)
       `(NUM ,(string-to-number argument))
     `(REG ,(aref argument 0))))
 
 (defun parse-instruction (instruction-line)
+  "Parse the instruction in INSTRUCTION-LINE."
   (pcase (split-string instruction-line " " t)
     (`("snd" ,x)
      `(SND ,(parse-argument x)))
@@ -39,45 +74,71 @@
            ,(parse-argument y)))))
 
 (defun parse-instructions (lines)
+  "Parse all instructions in LINES."
   (mapcar #'parse-instruction lines))
 
 (defun make-registers ()
-  (make-char-table 'registers))
+  "Make a register bank."
+  (make-vector 256 nil))
 
 (defmacro make-number (x)
+  "Create a number from X.
+
+Could be a bignum."
   x)
 
 (defun get-register (key register-bank)
+  "Get the value of register by nam KEY in REGISTER-BANK."
   (let ((lookup (aref register-bank key)))
     (if (null lookup)
         (make-number 0)
       lookup)))
 
 (defun set-register (key value register-bank)
+  "Set the value of register by name KEY to VALUE in REGISTER-BANK."
   (aset register-bank key value))
 
-(defvar last-played nil)
+(defvar last-played nil
+  "The last played sound.")
 
 (defun get-value (type val register-bank)
+  "Get a value.
+
+If TYPE is REG, then search for register by name VAL in
+REGISTER-BANK."
   (if (eq type 'REG)
       (get-register val register-bank)
     val))
 
-(defconst zero (make-number 0))
+(defconst zero (make-number 0)
+  "The number zero.")
 
 (defmacro add (x y)
+  "Add X and Y.
+
+Op could be subbed for bignum ops."
   `(+ ,x ,y))
 
 (defmacro subtract (x y)
+  "Subtract X and Y.
+
+Op could be subbed for bignum ops."
   `(- ,x ,y))
 
 (defmacro multiply (x y)
+  "Multiply X and Y.
+
+Op could be subbed for bignum ops."
   `(* ,x ,y))
 
 (defmacro modulo (x y)
+  "Produce the modulo of X and Y.
+
+Op could be subbed for bignum ops."
   `(mod ,x ,y))
 
 (defmacro compare (x y)
+  "Produce 1 if X is greater than Y, 0 if they're equal and -1 otherwise."
   `(if (> ,x ,y)
        1
      (if (< ,x ,y)
@@ -85,6 +146,7 @@
        0)))
 
 (defun execute-instruction (instruction register-bank)
+  "Execute INSTRUCTION with REGISTER-BANK."
   (pcase instruction
     (`(SND (,type ,x))
      (progn
@@ -124,13 +186,13 @@
     (`(RCV (,type ,x))
      (when (compare (get-value type x register-bank) zero)
        `(RCV ,last-played)))
-    (`(JGZ (,_ ,x)
-           (,type ,y))
-     (when (> (get-register x register-bank)
-              0)
-       `(JUMP ,y)))))
+    (`(JGZ (,x-type ,x)
+           (,y-type ,y))
+     (when (> (get-value x-type x register-bank) 0)
+       `(JUMP ,(get-value y-type y register-bank))))))
 
 (defun execute-instructions-to-recv (instructions)
+  "Execute INSTRUCTIONS and get the last played sound."
   (let ((register-bank (make-registers))
         (last-played nil)
         (instruction-vec (apply #'vector instructions))
@@ -158,14 +220,32 @@
          (last-sound   (execute-instructions-to-recv instructions)))
     (message "%s" last-sound)))
 
-;; Solution:
+;; Solution: 2951
 
 ;; Part 2
 
-(defvar channel-0 nil)
-(defvar channel-1 nil)
+(defvar channel-0 nil
+  "Transmition channel for thread 0 to send signals.")
+(defvar channel-1 nil
+  "Transmission channel for thread 1 to send segnals.")
+
+(defun is-blocking (last-instruction channel)
+  "Produce t if LAST-INSTRUCTION is a receive (RCV) and CHANNEL is empty (nil)."
+  (and (eq (car last-instruction) 'RCV)
+       (null channel)))
+
+(defvar send-count nil
+  "The number of times that thread one transmitted.")
+(defvar pointer-0 nil
+  "The pointer for program zero.")
+(defvar pointer-1 nil
+  "The pointer for program one.")
 
 (defun execute-instruction-transmition (instruction register-bank rec-channel snd-channel)
+  "Execute INSTRUCTION on REGISTER-BANK.
+
+Receives from other threads on REC-CHANNEL and sends to other
+threads on SND-CHANNEL."
   (pcase instruction
     (`(SND (,type ,x))
      (progn
@@ -210,28 +290,25 @@
            (set-register x (pop (symbol-value rec-channel)) register-bank)
            nil)
        '(RCV)))
-    (`(JGZ (,_ ,x)
-           (,type ,y))
-     (when (compare (get-register x register-bank) zero)
-       `(JUMP ,(get-value type y register-bank))))))
+    (`(JGZ (,x-type ,x)
+           (,y-type ,y))
+     (when (> (get-value x-type x register-bank) 0)
+       `(JUMP ,(get-value y-type y register-bank))))))
 
-(defun is-blocking (last-instruction channel)
-  (and (eq (car last-instruction) 'RCV)
-       (null channel)))
+(defun execute (program register-bank pointer program-length rec-channel snd-channel)
+  "Run PROGRAM with REGISTER-BANK at position POINTER.
 
-(defvar send-count nil)
-(defvar pointer-0 nil)
-(defvar pointer-1 nil)
+Program pointer is OOB at POINTER >= PROGRAM-LENGTH.
 
-(defun maybe-execute (program register-bank pointer last-instruction-result program-length)
-  (when (and (not (>= (symbol-value pointer) program-length))
-             (not (is-blocking last-instruction-result channel-0)))
-    (setq last-instruction-result
-          (execute-instruction-transmition (aref program
-                                                 (symbol-value pointer))
-                                           register-bank
-                                           'channel-0
-                                           'channel-1))
+Receive messages on REC-CHANNEL.
+
+Send messages on SND-CHANNEL."
+  (let ((last-instruction-result
+         (execute-instruction-transmition (aref program
+                                                (symbol-value pointer))
+                                          register-bank
+                                          rec-channel
+                                          snd-channel)))
     (when (and (eq (car last-instruction-result) 'SND)
                ;; A total hack but w/e
                (eq pointer 'pointer-1))
@@ -241,41 +318,42 @@
                  'JUMP))
         (let ((jump-value (cadr last-instruction-result)))
           (cl-incf (symbol-value pointer) (if (eq jump-value 0) 1 jump-value)))
-      (cl-incf (symbol-value pointer)))
+      (when (not (eq (car last-instruction-result) 'RCV))
+        (cl-incf (symbol-value pointer))))
     last-instruction-result))
 
-(defun execute-instructions-transmit (instructions)
+(defun execute-instructions-race (instructions)
+  "Execute INSTRUCTIONS on two threads.
+
+Thread zero should have register `p' set to 0 and thread one
+should have `p' set to 1 at the start."
   (let* ((register-bank-0 (make-registers))
          (register-bank-1 (make-registers))
          (channel-0 nil)
          (channel-1 nil)
          (send-count 0)
-         (instruction-vec (apply #'vector instructions))
+         (program (apply #'vector instructions))
          (pointer-0 0)
          (pointer-1 0)
-         (program-length (length instruction-vec))
+         (program-length (length program))
          (last-instruction-result-0 nil)
          (last-instruction-result-1 nil))
     (set-register ?p (make-number 0) register-bank-0)
     (set-register ?p (make-number 1) register-bank-1)
     (while (not (or (>= pointer-0 program-length)
-                    (and (or (>= pointer-0 program-length)
-                             (is-blocking last-instruction-result-0 channel-0))
-                         (or (>= pointer-1 program-length)
-                             (is-blocking last-instruction-result-1 channel-1)))))
-      (setq last-instruction-result-0
-            (maybe-execute instruction-vec
-                           register-bank-0
-                           'pointer-0
-                           last-instruction-result-0
-                           program-length))
-      (setq last-instruction-result-1
-            (maybe-execute instruction-vec
-                           register-bank-1
-                           'pointer-1
-                           last-instruction-result-1
-                           program-length)))
-    (message "Zero: %s" pointer-0)
+                    (>= pointer-1 program-length)
+                    (and (is-blocking last-instruction-result-0 channel-0)
+                         (is-blocking last-instruction-result-1 channel-1))))
+      (while (not (or (>= pointer-0 program-length)
+                      (is-blocking last-instruction-result-0 channel-1)))
+        (setq last-instruction-result-0
+              (execute program register-bank-0 'pointer-0
+                       program-length 'channel-1 'channel-0)))
+      (while (not (or (>= pointer-1 program-length)
+                      (is-blocking last-instruction-result-1 channel-0)))
+        (setq last-instruction-result-1
+              (execute program register-bank-1 'pointer-1
+                       program-length 'channel-0 'channel-1))))
     send-count))
 
 (defun day18-part-2 (input)
@@ -283,40 +361,12 @@
   (interactive "sInput: ")
   (let* ((lines (parse-lines input))
          (instructions (parse-instructions lines))
-         (send-count  (execute-instructions-transmit instructions)))
+         (send-count  (execute-instructions-race instructions)))
     (message "%s" send-count)))
 
-;; Solution: 
+;; Wrong solution: 7493 (?) (was counting the wrong sends)
+
+;; Solution: 7366
 
 (provide 'day18)
 ;;; day18 ends here
-
-
-;; (when (and (not (> pointer-0 program-length))
-;;                  (not (is-blocking last-instruction-result-0 channel-0)))
-;;         (setq last-instruction-result-0
-;;               (execute-instruction-transmition (aref instruction-vec
-;;                                                      pointer-0)
-;;                                                register-bank-0
-;;                                                'channel-0
-;;                                                'channel-1))
-;;         (when (eq (car last-instruction-result-0) 'SND)
-;;           (cl-incf send-count))
-;;         (if (and last-instruction-result-0
-;;                  (eq (car last-instruction-result-0)
-;;                      'JUMP))
-;;             (setq pointer-0 (+ pointer-0 (cadr last-instruction-result-0)))
-;;           (cl-incf pointer-0)))
-;;       (when (and (not (> pointer-1 program-length))
-;;                  (not (is-blocking last-instruction-result-1 channel-1)))
-;;         (setq last-instruction-result-1
-;;               (execute-instruction-transmition (aref instruction-vec
-;;                                                      pointer-1)
-;;                                                register-bank-1
-;;                                                'channel-1
-;;                                                'channel-0))
-;;         (if (and last-instruction-result-1
-;;                  (eq (car last-instruction-result-1)
-;;                      'JUMP))
-;;             (setq pointer-1 (+ pointer-1 (cadr last-instruction-result-1)))
-;;           (cl-incf pointer-1)))
